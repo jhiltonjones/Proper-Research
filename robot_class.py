57f16a6a4089171bc76ae6ddff3e21cb4052c33c
Jack Hilton-Jones <jack.hiltonjones@gmail.com>
2025-12-09 14:44:37 +0000

    added the robo class with all basic features and home

---
+++ robot_class.py
@@ -0,0 +1,46 @@
+import rtde_control
+import rtde_receive 
+
+
+class URRtde:
+
+    def __init__(self, ip: str):
+        self.ip = ip
+        print(f"Connecting to Robo at the ip: {ip}")
+        self.rtde_c = rtde_control.RTDEControlInterface(ip)
+        self.rtde_r = rtde_receive.RTDEReceiveInterface(ip)
+        print("Connected with Robo")
+
+    def shutdown(self):
+        try:
+            self.rtde_c.stopScript()
+        except Exception as e:
+            print(f"Shutdown warning {e}")
+        try:
+            self.rtde_c.disconnect()
+        except Exception as e:
+            print(f"Disconnect warning for the connect module {e}")
+        try: 
+            self.rtde_r.disconnect()
+        except Exception as e:
+            print(f"Disconnect warning for the receive module {e}")
+        print("Robo is resting")
+    
+    def get_joints(self):
+        q = self.rtde_r.getActualQ()
+        print(f"State Joints: {q}")
+        return q
+    
+    def get_pose(self):
+        pose = self.rtde_r.getActualTCPPose()
+        print(f"TCP pose: {pose}")
+        return pose
+    
+    def moveJ(self, q, speed = 0.7, accel = 0.7, blend_radius = 0.0):
+        self.rtde_c.moveJ(q, speed, accel, blend_radius)
+    
+    def moveL(self, pose, speed=0.25, accel = 0.5):
+        self.rtde_c.moveL(pose, speed, accel)
+
+    def go_home(self, target_tcp= [0.528583395185381, -0.41938710566998094, 0.4502478513259566, -1.9773976132360036, 2.420306911735209, -0.016056316733630456]):
+        self.moveL(target_tcp, speed=0.1, accel=0.2)
